2a78731560abb7b930acb99b221910f696702887
程金盛 <jelly@chengjinshengdeMacBook-Pro.local>
2016-06-12 22:41:55 +0800

    support

---
+++ src/Support/Helpers.php
@@ -0,0 +1,116 @@
+<?php
+namespace JellySupport;
+/**
+ * 助手函数
+ */
+
+function randStr($length = 16){
+    // 密码字符集，可任意添加你需要的字符
+    $chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    $str = "";
+    for($i = 0; $i < $length; $i++)
+    {
+        $str .= $chars[mt_rand(0, strlen($chars) - 1)];
+    }
+    return $str;
+}
+
+/**
+ * 判断是否SSL协议
+ * @return boolean
+ */
+function is_ssl() {
+    if(isset($_SERVER['HTTPS']) && ('1' == $_SERVER['HTTPS'] || 'on' == strtolower($_SERVER['HTTPS']))){
+        return true;
+    }elseif(isset($_SERVER['SERVER_PORT']) && ('443' == $_SERVER['SERVER_PORT'] )) {
+        return true;
+    }
+    return false;
+}
+
+
+
+/**
+ * XML编码
+ * @param mixed $data 数据
+ * @param string $root 根节点名
+ * @param string $item 数字索引的子节点名
+ * @param string $attr 根节点属性
+ * @param string $id 数字索引子节点key转换的属性名
+ * @param string $encoding 数据编码
+ * @return string
+ */
+function xml_encode($data, $root = 'ectouch', $item = 'item', $attr = '', $id = 'id', $encoding = 'utf-8') {
+    if (is_array($attr)) {
+        $_attr = array();
+        foreach ($attr as $key => $value) {
+            $_attr[] = "{$key}=\"{$value}\"";
+        }
+        $attr = implode(' ', $_attr);
+    }
+    $attr = trim($attr);
+    $attr = empty($attr) ? '' : " {$attr}";
+    $xml = "<?xml version=\"1.0\" encoding=\"{$encoding}\"?>";
+    $xml .= "<{$root}{$attr}>";
+    $xml .= data_to_xml($data, $item, $id);
+    $xml .= "</{$root}>";
+    return $xml;
+}
+/**
+ * 数据XML编码
+ * @param mixed $data 数据
+ * @param string $item 数字索引时的节点名称
+ * @param string $id 数字索引key转换为的属性名
+ * @return string
+ */
+function data_to_xml($data, $item = 'item', $id = 'id') {
+    $xml = $attr = '';
+    foreach ($data as $key => $val) {
+        if (is_numeric($key)) {
+            $id && $attr = " {$id}=\"{$key}\"";
+            $key = $item;
+        }
+        $xml .= "<{$key}{$attr}>";
+        $xml .= (is_array($val) || is_object($val)) ? data_to_xml($val, $item, $id) : $val;
+        $xml .= "</{$key}>";
+    }
+    return $xml;
+}
+
+
+/**
+ * 字符串命名风格转换
+ * type 0 将Java风格转换为C的风格 1 将C风格转换为Java的风格
+ * @param string $name 字符串
+ * @param integer $type 转换类型
+ * @return string
+ */
+function parseName($name, $type=0) {
+    if ($type) {
+        return ucfirst(preg_replace("/_([a-zA-Z])/e", "strtoupper('\\1')", $name));
+    } else {
+        return strtolower(trim(preg_replace("/[A-Z]/", "_\\0", $name), "_"));
+    }
+}
+
+function redirect($url, $time=0, $msg='') {
+    if (empty($msg)) {
+        $msg    = "系统将在{$time}秒之后自动跳转到{$url}！";
+    }
+    if (!headers_sent()) {
+        if (0 === $time) {
+            header('Location: ' . $url);
+        } else {
+            header("refresh:{$time};url={$url}");
+            echo($msg);
+        }
+        exit();
+    } else {
+        $str    = "<meta http-equiv='Refresh' content='{$time};URL={$url}'>";
+        if ($time != 0) {
+            $str .= $msg;
+        }
+        exit($str);
+    }
+}
+
