f157de4b03368080d4af02efa3b62cc67bb547b2
cjs <42282367@qq.com>
2017-01-21 17:58:46 +0800

    empty cls

---
+++ src/Support/EmptyImpl.php
@@ -0,0 +1,22 @@
+<?php
+namespace CjsSupport;
+
+/**
+ * 空实现
+ */
+class EmptyImpl{
+
+    public static function create()
+    {
+        static $instance = null;
+        if(is_null($instance)) {
+            $instance = new static();
+        }
+        return $instance;
+    }
+
+    public function __call($method, $args) {
+        return null;
+    }
+
+}
