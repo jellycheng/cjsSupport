87a1e89ba8dc84e1e434d5057ffb59ca673851f8
程金盛 <jelly@chengjinshengdeMacBook-Pro.local>
2017-02-19 21:52:17 +0800

    expression

---
+++ src/Support/Expression.php
@@ -0,0 +1,26 @@
+<?php
+namespace CjsSupport;
+/**
+ * 表达式
+ */
+class Expression
+{
+
+    protected $value;
+
+    public function __construct($value)
+    {
+        $this->value = $value;
+    }
+
+    public function getValue()
+    {
+        return $this->value;
+    }
+
+    public function __toString()
+    {
+        return (string) $this->getValue();
+    }
+
+}
