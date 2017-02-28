c002e6db6ae55d24650e873b468e297c1643e423
cjs <42282367@qq.com>
2017-02-28 21:21:43 +0800

    cjs fun

--- src/Support/Helpers.php
+++ src/Support/Helpers.php
@@ -267,5 +267,21 @@ $trace
 EOF;
 }
 
+function json_encode($data, $options=null)
+{
+    if(is_null($options)) {
+        $options = 0;
+        if (defined('JSON_UNESCAPED_SLASHES')) {
+            $options |= JSON_UNESCAPED_SLASHES;
+        }
+        if (defined('JSON_UNESCAPED_UNICODE')) {
+            $options |= JSON_UNESCAPED_UNICODE;
+        }
+    }
+    return \json_encode($data, $options);
+}
 
+function with($object) {
+    return $object;
+}
 
